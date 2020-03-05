3ed9bb88396339e93f32a1bed1149a15b0bde31b
LucileLinh <lenz213@hotmail.com>
2020-03-05 12:20:45 +0100

    init client app and routes

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+const express = require("express")
+const mongoose = require("mongoose")
+const bodyParser = require("body-parser")
 
-const app = express();
+// IMPORT MODELS
+require("./models/Product")
 
-mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost:27017/node-react-starter`);
+const app = express()
 
-app.use(bodyParser.json());
+mongoose.Promise = global.Promise
+mongoose.connect(
+  process.env.MONGODB_URI || `mongodb://localhost:27017/node-react-starter`,
+)
 
-const PORT = process.env.PORT || 5000;
+app.use(bodyParser.json())
+
+//IMPORT ROUTES
+require("./routes/productRoutes")(app)
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"))
+
+  const path = require("path")
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  })
+}
+
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
   console.log(`app running on port ${PORT}`)
-});
+})
